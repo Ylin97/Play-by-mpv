90712e999f81bdf41165dabe16f80b49fc4b700c
Ylin <yalin19970625@gmail.com>
2021-05-29 23:37:56 +0800

    Initial project & realize to play real url by mpv

---
+++ test.py
@@ -0,0 +1,10 @@
+import os 
+
+
+def test_cmd():
+    cmd = 'mpv '
+    cmd += 'https://xy111x78x163x90xy.mcdn.bilivideo.cn:4483/upgcxcode/77/69/337086977/337086977-1-30077.m4s\?e\=ig8euxZM2rNcNbdlhoNvNC8BqJIzNbfqXBvEqxTEto8BTrNvN0GvT90W5JZMkX_YN0MvXg8gNEV4NC8xNEV4N03eN0B5tZlqNxTEto8BTrNvNeZVuJ10Kj_g2UB02J0mN0B5tZlqNCNEto8BTrNvNC7MTX502C8f2jmMQJ6mqF2fka1mqx6gqj0eN0B599M\=\&uipk\=5\&nbs\=1\&deadline\=1622299972\&gen\=playurlv2\&os\=mcdn\&oi\=2936350555\&trid\=0001217188d0854d49d38318d0a222f0857ap\&platform\=pc\&upsig\=23172bdc301ed1558c71d8b328c5c79e\&uparams\=e,uipk,nbs,deadline,gen,os,oi,trid,platform\&mcdnid\=2000558\&mid\=0\&bvc\=vod\&orderid\=0,3\&agrr\=0\&logo\=A0000002 audio-file=https://xy182x105x223x218xy.mcdn.bilivideo.cn:4483/upgcxcode/77/69/337086977/337086977_nb2-1-30280.m4s\?e\=ig8euxZM2rNcNbdlhoNvNC8BqJIzNbfqXBvEqxTEto8BTrNvN0GvT90W5JZMkX_YN0MvXg8gNEV4NC8xNEV4N03eN0B5tZlqNxTEto8BTrNvNeZVuJ10Kj_g2UB02J0mN0B5tZlqNCNEto8BTrNvNC7MTX502C8f2jmMQJ6mqF2fka1mqx6gqj0eN0B599M\=\&uipk\=5\&nbs\=1\&deadline\=1622299972\&gen\=playurlv2\&os\=mcdn\&oi\=2936350555\&trid\=0001217188d0854d49d38318d0a222f0857ap\&platform\=pc\&upsig\=bdf425d90bb49af0782c2ed28f4e0914\&uparams\=e,uipk,nbs,deadline,gen,os,oi,trid,platform\&mcdnid\=2000691\&mid\=0\&bvc\=vod\&orderid\=0,3\&agrr\=0\&logo\=A0000002 --referrer=https://www.bilibili.com --no-ytdl'
+    os.system(cmd)
+
+if __name__ == '__main__':
+    test_cmd()
